b11f102da9f6bab5263a0461204f097bb7dfd5f4
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-19 17:40:49 -0300

    Inserido python-decouple.

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lb(rn5(kpk)c6zbtrga)7a4885c7z*h_8e6!*r)lt@ed!k)t35'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
