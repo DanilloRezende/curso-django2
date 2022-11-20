000f82cd2df42b8c27f3ab410c9b174a079cf64f
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-20 13:43:00 -0300

    Instalado Bootstrap

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<html><body>Olá Django</html></body>', content_type='text/html')
+    return render(request, 'base/home.html')
