8465ee003b59ee36a96e7721b3a4a5591eb6fc2d
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-22 17:12:25 -0300

    Video de Acordo com Slug.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 def video(request, slug):
-    video={'titulo': 'Video Aperitivo: Motivação'}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'youtube_id': 'Lgh8JgcYFwM'},
+        'projetos-django': {'titulo': 'Video Projetos DJANGO', 'youtube_id': 'l9d4BcwQsbQ'}
+    }
+    video= videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
