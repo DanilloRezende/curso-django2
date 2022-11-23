48d5ffded55f91e19eaa8dabe6e34eecaccb5446
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-23 17:33:09 -0300

    Composição de Templates

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,25 +1,15 @@
 from django.shortcuts import render
-from django.urls import reverse
 
-
-class Video:
-    def __init__(self, slug, titulo, youtube_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.youtube_id = youtube_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
+from pypro.aperitivos.models import Video
 
 videos = [
-    Video('motivacao', 'Video Aperitivo: Motivação', 'Lgh8JgcYFwM'),
-    Video('projetos-django', 'Video Projetos DJANGO', 'l9d4BcwQsbQ')
+    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', youtube_id='Lgh8JgcYFwM'),
+    Video(slug='projetos-django', titulo='Video Projetos DJANGO', youtube_id='l9d4BcwQsbQ')
 ]
 
 videos_dct = {v.slug: v for v in videos}
 def indice(request):
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 def video(request, slug):
-    video= videos_dct[slug]
+    video= Video.objects.get(slug=slug )
     return render(request, 'aperitivos/video.html', context={'video': video})
