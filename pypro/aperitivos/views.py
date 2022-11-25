a3aaf4735ff333023da5a1ecaf65ed739f590af8
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-25 17:33:00 -0300

    Introdução a QuerySet e Migrações

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -11,5 +11,5 @@ videos_dct = {v.slug: v for v in videos}
 def indice(request):
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 def video(request, slug):
-    video= Video.objects.get(slug=slug )
+    video= Video.objects.get(slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
