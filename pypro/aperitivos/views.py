bd287e8e201dfdebd33a0515e83a5f709f1ecf0f
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-26 21:37:43 -0300

    Testes com Banco de Dados, indice.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,13 +2,8 @@ from django.shortcuts import render, get_object_or_404
 
 from pypro.aperitivos.models import Video
 
-videos = [
-    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', youtube_id='Lgh8JgcYFwM'),
-    Video(slug='projetos-django', titulo='Video Projetos DJANGO', youtube_id='l9d4BcwQsbQ')
-]
-
-videos_dct = {v.slug: v for v in videos}
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 def video(request, slug):
     video= get_object_or_404(Video, slug=slug)
