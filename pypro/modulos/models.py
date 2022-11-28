75bbf2e770328dc345e0769479d0df57c0a66d4a
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-28 16:57:48 -0300

    Correção do menu modulos.

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from ordered_model.models import OrderedModel
 
-class Modulo(models.Model):
+
+class Modulo(OrderedModel):
     titulo=models.CharField(max_length=64)
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def __str__(self):
+        return self.titulo
