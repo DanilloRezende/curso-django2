155d961b22453f2268b705df78944b0b998b1e57
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-28 10:53:30 -0300

    Implementados modelos nos testes..

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -1,18 +1,30 @@
 import pytest
 from django.urls import reverse
 
+from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
 
+@pytest.fixture
+def video(db):
+    v = Video(slug='motivacao', titulo='Video Aperitivo: Motivação', youtube_id='Lgh8JgcYFwM')
+    v.save()
+    return v
+@pytest.fixture
+def resp(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug,)))
 
 @pytest.fixture
-def resp(client):
-    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
+
+def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
-def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+def test_titulo_video(resp, video):
+    assert_contains(resp, video.titulo)
 
 def teste_conteudo_video(resp):
     assert_contains(resp, """<iframe
