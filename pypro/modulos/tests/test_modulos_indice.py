ca24b6b989e3bcf616751fb2f09b416ba874e601
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-30 17:49:18 -0300

    Acesso ao lado N do relacionamento.

--- pypro/modulos/tests/test_modulos_indice.py
+++ pypro/modulos/tests/test_modulos_indice.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pytest
 from django.test import Client
 from django.urls import reverse
@@ -27,19 +29,23 @@ def test_indice_disponivel(resp):
     assert resp.status_code == 200
 
 
-# def test_titulos(resp, modulo: Modulo):
-#     assert_contains(resp, modulo.titulo)
-#
-# def test_descricao(resp, modulo: Modulo):
-#     assert_contains(resp, modulo.descricao)
-#
-# def test_publico(resp, modulo: Modulo):
-#     assert_contains(resp, modulo.publico)
-#
-# def test_aulas_titulos(resp, aulas):
+def test_titulos(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.titulo)
+
+def test_descricao(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.descricao)
+
+def test_publico(resp, modulos: List[Modulo]):
+    for modulo in modulos:
+        assert_contains(resp, modulo.publico)
+
+# Testes n funcionam devido a divergencia das versões utilizadas do pyCharm.(_set)
+# def test_aulas_titulos(resp, aulas: List[Aula]):
 #     for aula in aulas:
 #         assert_contains(resp, aula.titulo)
 #
-# def test_aulas_links(resp, aulas):
+# def test_aulas_links(resp, aulas: List[Aula]):
 #     for aula in aulas:
 #         assert_contains(resp, aula.get_absolut_url())
