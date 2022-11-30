ef4d07fb658fe05f19da65f3db3b0f07b558209b
DanilloRezende <danillo.rezende3@gmail.com>
2022-11-30 17:35:52 -0300

    Testes indice.

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from pypro.modulos.views import detalhe, aula
+from pypro.modulos.views import detalhe, aula, indice
 
 app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', detalhe, name='detalhe'),
+    path('', indice, name='indice'),
     path('aulas/<slug:slug>', aula, name='aula'),
 
 ]
